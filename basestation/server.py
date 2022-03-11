fa184ed5e5c5fc0d9c668ac96ae62685e188f4f3
djpardo <79879311+djpardo@users.noreply.github.com>
2022-03-11 13:37:47 -0500

    Update server.py

--- basestation/server.py
+++ basestation/server.py
@@ -1,1 +1,16 @@
+import bottle
+import art
 
+@bottle.route("/")
+def html():
+  return bottle.static_file("index.html", root="")
+
+@bottle.route("/map.js")
+def js():
+  return bottle.static_file("map.js", root="")
+
+@bottle.route("/art")
+def get_art_data():
+  return art.process_art_data('https://data.buffalony.gov/resource/v5df-q4ru.json')
+
+bottle.run(host="0.0.0.0", port=8080, debug=True)
